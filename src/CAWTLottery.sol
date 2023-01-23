8ed8bfbd94e4d9242b3be598aba8c717fac079c3
lyov077 <lyov.jkl02@gmail.com>
2023-01-23 19:59:55 +0400

    Start lottery development

---
+++ src/CAWTLottery.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.8.17;
+
+contract CAWTLottery {
+    function createLottery() public {
+        //create lottery
+    }
+
+    function participate() public {
+        //participate in lottery
+    }
+
+    function sendPrize() public {
+        //send prize to winner
+    }
+
+    function setToken(address _token) external {
+        //set token
+    }
+    
+}
